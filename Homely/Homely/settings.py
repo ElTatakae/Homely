be3fdce75679475674af6b4e97600f04a51799de
Eduardo <calle13.201@gmail.com>
2024-05-20 13:01:58 -0600

    Configuracion de catalogos.context_processors.categorias

--- Homely/Homely/settings.py
+++ Homely/Homely/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'catalogos.context_processors.categorias',  #context_processors para categorias
             ],
         },
     },
