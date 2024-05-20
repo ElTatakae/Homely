01802ea60d51acdd2a45a0c16ff6ce67dc04deff
Eduardo <calle13.201@gmail.com>
2024-05-20 12:18:21 -0600

    Creacion de modelos para servicios

--- Homely/catalogos/models.py
+++ Homely/catalogos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+class Servicio(models.Model):
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    imagen = models.ImageField(upload_to='servicios/')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.descripcion
