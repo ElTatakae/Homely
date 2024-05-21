b1261728932cf915941dd36adb601d2d9167f876
Eduardo <calle13.201@gmail.com>
2024-05-20 22:13:40 -0600

    sobreescritura

--- Homely/catalogos/models.py
+++ Homely/catalogos/models.py
@@ -7,10 +7,11 @@ class Categoria(models.Model):
         return self.nombre
 
 class Servicio(models.Model):
+    nombre = models.CharField(max_length=200, default='nombre servicio')  
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=6, decimal_places=2)
     imagen = models.ImageField(upload_to='servicios/')
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.descripcion
+        return f"{self.nombre} - {self.categoria} - ${self.precio}"
